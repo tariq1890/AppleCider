43a330bdac176b3cbe9d8cf166c01edae228e9b2
Thomas Kwak <teekwak@gmail.com>
2017-08-11 08:24:48 -0700

    Java program for splitting data by new task

---
+++ FileSplitter/Splitter.java
@@ -0,0 +1,48 @@
+import java.io.*;
+import java.util.*;
+
+public class Splitter {
+	private int counter;
+
+	public Splitter() {
+		this.counter = 1;
+	}
+
+	public void splitFileByNewTask(String fileName) {
+		List<String> taskLines = new ArrayList<>();
+
+		try(BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(fileName)))) {
+			for(String line; (line = br.readLine()) != null; ) {
+				if(line.startsWith("NewTask")) {
+					if(taskLines.size() > 0) {
+						this.writeListToFile(taskLines);
+					}
+
+					taskLines = new ArrayList<>();
+					taskLines.add(line);
+				} else {
+					taskLines.add(line);
+				}
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void writeListToFile(List<String> list) {
+		try(BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(new FileOutputStream("./splits/" + counter + ".csv", true)))) {
+			for(String line : list) {
+				bw.write(line + "\n");
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		this.counter++;
+	}
+
+	public static void main(String[] args) {
+		Splitter obj = new Splitter();
+		obj.splitFileByNewTask("./SpecialGoogleFinal.csv");
+	}
+}
